dad5553459783a183085308efdf81e301e1dfe00
Bhupendra jangid <bhupendrajangid34@gmail.com>
2025-03-16 12:26:55 +0530

    complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Icon } from "@iconify/react";
+
 
 function Navbar() {
   return (
     <div className='sticky top-0'>
-      <header className='border border-black'>
-        <nav className='flex justify-between px-24 items-center p-2 bg-white'>
+      <header className='border border-black '>
+        <nav className='flex justify-between px-24 items-center p-2 bg-white '>
           <div><h2 className='font-bold text-[25px]'>LOGO</h2></div>
-           <ul className='flex justify-between border-none border-black w-[450px] font-semibold text-black'>
+           <ul className='flex justify-between border-none border-black w-[450px] font-semibold text-black max-sm:hidden'>
               <li><a>Home</a></li>
               <li><a>About</a></li>
               <li><a>Skill</a></li>
@@ -16,6 +18,7 @@ function Navbar() {
               
            </ul>
            <div>
+
             <a><button className=' px-7 py-2  rounded-3xl bg-purple-500 text-white font-semibold '>Button</button></a>
            </div>
         </nav>
