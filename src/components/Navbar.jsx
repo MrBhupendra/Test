6841e1718e98f4535e5c7c84d7d949b94bbeee5b
Bhupendra jangid <bhupendrajangid34@gmail.com>
2025-07-03 17:40:38 +0530

    Update Navbar.jsx
    complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ import { Icon } from "@iconify/react";
 function Navbar() {
   return (
     <div className='sticky top-0 '>
-      <header className='Navbar  '>
-        <nav className='flex justify-between px-24 items-center p-2 bg-white  max-sm:w-auto max-sm:flex max-sm:justify-between  max-sm:px-3'>
-          <h2 className='font-bold text-[25px]'>LOGO</h2>
+      <header className='header bg-white w-full sticky top-0 px-24  max-sm:px-2 z-40 '>
+        <nav className='border-b border-slate-400  flex justify-between  items-center p-2   max-sm:w-auto max-sm:flex max-sm:justify-between  max-sm:px-3'>
+          <h2 className='font-bold text-[25px]'>PORTFOLIO</h2>
            <ul className='flex justify-between border-none border-black w-[450px] font-semibold text-black max-sm:hidden '>
               <li><a>Home</a></li>
               <li><a>About</a></li>
@@ -19,7 +19,7 @@ function Navbar() {
            </ul>
            <div>
 
-            <a><button className=' px-7 py-2  rounded-3xl bg-purple-500 text-white font-semibold '>Button</button></a>
+            <a><button className=' px-7 py-2  rounded-3xl bg-purple-500 text-white font-semibold '>Get Sarted</button></a>
            </div>
         </nav>
       </header>
