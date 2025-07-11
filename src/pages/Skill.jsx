d9fb9b8d2f1fb194ec93e2bc4047033dbb00d7c3
Bhupendra jangid <bhupendrajangid34@gmail.com>
2025-07-11 13:22:07 +0530

    Update Skill.jsx
    code push

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -15,6 +15,59 @@ function Skill() {
       </div>
 
       <div className='skill flex px-8 py-4  gap-6  max-sm:flex-col max-sm:mt-5 '>
+        
+         <div className='shadow-md flex flex-col items-center px-4 py-10 rounded-sm bg-white'>
+            <div className=' border border-red-300 w-[43vh]'>
+              <div className='flex justify-between items-center border border-blue-500 '>
+               <div className='html text-[14px] rounded-md w-full flex justify-between border border-black font-semibold'>
+                 <p className=''>HTML/CSS</p>
+                 <div className='text-sm'>90%</div>
+               </div>
+                  <div className='flex justify-end' >
+                    <Icon className='text-2xl' icon="skill-icons:html" />
+                  </div>
+              </div>
+             
+
+            <div className='flex justify-between items-center border border-blue-500 '>
+               <div className='Tailwind text-[14px] rounded-md w-full flex justify-between border border-black font-semibold '>
+                 <p className=''>TAILWIND CSS</p>
+                 <div className='text-sm'>90%</div>
+               </div>
+               <div className='flex justify-end' >
+                  <Icon className='text-2xl' icon="skill-icons:tailwindcss-dark" />
+               </div>
+            </div>
+
+
+              <div className='flex justify-between items-center border border-blue-500 '>
+              <div className='Javascript flex justify-between border border-black font-semibold text-[14px] rounded-md'>
+                 <p className=''>JAVASCRIPT</p>
+                 <div className='text-sm'>90%</div>
+              </div>
+               <div>
+                 <Icon className='text-[25px]' icon="skill-icons:javascript" />
+
+               </div>
+               </div>
+              
+              
+               <div className='flex justify-between border border-black font-serif'>
+                 <p className=''>REACT JS</p>
+                 <div>70%</div>
+                <div>
+
+                 </div>
+                 <Icon className='text-[25px]' icon="skill-icons:html" />
+
+              </div>
+             
+              
+            </div>
+
+
+        </div>
+
         <div className='shadow-md flex-col  items-center flex px-4  py-10 rounded-sm bg-white hover:bg-purple-500 hover:text-white'>
            <Icon className='text-4xl ' icon="simple-icons:nextui"></Icon>
            <h1 className='font-semibold text-3xl mt-3.5'>UI/UX Desgin</h1>
@@ -22,13 +75,6 @@ function Skill() {
            <button className='font-medium text-white hover:bg-white hover:text-black  bg-black px-18 p-1.5 text-[14px] rounded-sm mt-6'>See More</button>
         </div>
 
-        <div className='shadow-md flex flex-col items-center px-4 py-10 rounded-sm bg-white hover:bg-purple-500 hover:text-white'>
-          <Icon  className='text-5xl' icon="mdi:web-box"></Icon>
-          <h1 className='font-semibold text-3xl mt-1'>Wab Design</h1>
-           <p className='font-medium text-black text-[13px] mt-3 text-center'>I design intuitive, user-friendly, and visually engaging interfaces that enhance user experiences, improve accessibility, and drive engagement by blending creativity, functionality, and usability to create seamless digital interactions that delight users</p>
-           <button className='font-medium text-white  hover:bg-white hover:text-black  bg-black px-18 py-1.5 text-[14px] rounded-sm mt-6'>See More</button>
-           
-        </div>
         <div className='shadow-md flex-col  items-center flex px-4 py-10 rounded-sm bg-white hover:bg-purple-500 hover:text-white'>
           <Icon  className='text-5xl' icon="formkit:wordpress" ></Icon>
           <h1 className='font-semibold text-3xl mt-2'>WordPress</h1>
@@ -42,5 +88,5 @@ function Skill() {
   )
 }
 
-export default Skill
+export default Skill;
  
